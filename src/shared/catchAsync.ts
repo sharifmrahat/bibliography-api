9e6efb6d67be54dacd52faeaa1fb55c403372071
sharifmrahat <sharifmrahat@gmail.com>
2023-10-08 15:17:38 +0600

    error interface and shared configs is added

---
+++ src/shared/catchAsync.ts
@@ -0,0 +1,14 @@
+import { Request, RequestHandler, Response } from "express";
+import { NextFunction } from "express-serve-static-core";
+
+const catchAsync = (fn: RequestHandler) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await fn(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+};
+
+export default catchAsync;
